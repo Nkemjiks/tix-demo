5f6995f126d9132c4c05c96490a5e35625141fbe
Blessing Mbonu <mbonunkemjika@ymail.com>
2020-07-14 11:01:25 +0100

    Change port and update route file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'events/index'
-  get 'events/create'
-  get 'events/update'
-  get 'events/delete'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "events#index"
+  resources :events, only: [:create, :update, :delete]
 end
